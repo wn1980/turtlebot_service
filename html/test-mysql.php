5bd1eeca022f879ae0a22a626c34085d201c8d7c
wn1980 <w.n1980@outlook.com>
2019-09-08 15:54:32 +0700

    ok

--- html/test-mysql.php
+++ html/test-mysql.php
@@ -1,12 +1,12 @@
 <h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php 
+
 $host = 'db';
 $user = 'root';
 $pass = '123456';
-$db = 'mysql';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
